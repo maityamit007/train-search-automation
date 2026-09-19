ea8f4ec8cd4c0893cc2ee009d38c4e3691d5576b
maityamit007 <mitwaamit007@gmail.com>
2026-09-19 16:29:22 +0530

    new updates

--- tests/pnrStatusCheck.spec.js
+++ tests/pnrStatusCheck.spec.js
@@ -2,15 +2,12 @@ const { test, chromium } = require('@playwright/test');
 const { PnrStatusCheck } = require('../pages/pnrStatusCheck');
 require('dotenv').config();
 
-test('PNR Status Check', async () => {
-  
-  const browser = await chromium.launch({ headless: false, slowMo: 100 });
-  const page = await browser.newPage();
-  await page.goto(`https://www.confirmtkt.com/pnr-status/${process.env.PNR}`, {
-    waitUntil: 'domcontentloaded'
-  });
+test('PNR Status Check', async ({ page }) => {
+  await page.goto(`https://www.confirmtkt.com/pnr-status/${process.env.PNR}`,
+    { waitUntil: 'domcontentloaded' });
   const pnrStatusCheck = new PnrStatusCheck(page);
   await pnrStatusCheck.getCurrentWaitingNumbers();
-  await page.pause(); 
-  await browser.close();
+  if (!process.env.CI) {
+    await page.pause();
+  }
 });
